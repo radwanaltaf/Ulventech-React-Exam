ab7a5471e132907b56d2c7ce3e2acdcef220a017
radwanaltaf <radwanaltaf2000@gmail.com>
2022-03-08 08:35:30 +0800

    alignment

--- src/components/dynamic-form.tsx
+++ src/components/dynamic-form.tsx
@@ -36,45 +36,57 @@ export default function DynamicForm(): JSX.Element {
         // result.user => 'Ada Lovelace'
     }
 
+    const renderFormField = () => {
+        return (
+            <section id="mainForm">
+                <form onSubmit={onFormSubmit}>
+                    {formData ? formData.data.map((field, index) => {
+                        if (field.type === 'select') {
+                            return (
+                                <div key={index} className="form-group mb-3">
+                                    <label htmlFor={field.fieldName}>{field.fieldName}</label>
+                                    <select defaultValue={field.value} className="form-control" id={field.fieldName}>
+                                        {field.options?.map((option, i) => {
+                                            return (
+                                                <option key={i} value={option}>{option}</option>
+                                            )
+                                        })}
+                                    </select>
+                                </div>
+                            )
+                        } else if (field.type === 'multiline') {
+                            return (
+                                <div key={index} className="mb-3">
+                                    <label htmlFor={field.fieldName} className="form-label">{field.fieldName}
+                                    </label>
+                                    <textarea className="form-control" id={field.fieldName} name={field.fieldName} defaultValue={field.value} />
+                                </div>
+                            )
+                        } else {
+                            return (
+                                <div key={index} className="mb-3">
+                                    <label htmlFor={field.fieldName} className="form-label">{field.fieldName}
+                                    </label>
+                                    <input defaultValue={field.value} type={field.type} className="form-control" id={field.fieldName} name={field.fieldName} />
+                                </div>
+                            )
+    
+                        }
+                    }) : null}
+    
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </section>
+        )
+    }
+
     return (
-        <section id="mainForm">
-            <form onSubmit={onFormSubmit}>
-                {formData ? formData.data.map((field, index) => {
-                    if (field.type === 'select') {
-                        return (
-                            <div key={index} className="form-group mb-3">
-                                <label htmlFor={field.fieldName}>{field.fieldName}</label>
-                                <select defaultValue={field.value} className="form-control" id={field.fieldName}>
-                                    {field.options?.map((option, i) => {
-                                        return (
-                                            <option key={i} value={option}>{option}</option>
-                                        )
-                                    })}
-                                </select>
-                            </div>
-                        )
-                    } else if (field.type === 'multiline') {
-                        return (
-                            <div key={index} className="mb-3">
-                                <label htmlFor={field.fieldName} className="form-label">{field.fieldName}
-                                </label>
-                                <textarea className="form-control" id={field.fieldName} name={field.fieldName} defaultValue={field.value} />
-                            </div>
-                        )
-                    } else {
-                        return (
-                            <div key={index} className="mb-3">
-                                <label htmlFor={field.fieldName} className="form-label">{field.fieldName}
-                                </label>
-                                <input defaultValue={field.value} type={field.type} className="form-control" id={field.fieldName} name={field.fieldName} />
-                            </div>
-                        )
-
-                    }
-                }) : null}
-
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </section>
+        <div className="d-flex align-items-center justify-content-center">
+            <div className="w-50">
+            {renderFormField()}
+            </div>
+        </div>
     )
+
+
 }
